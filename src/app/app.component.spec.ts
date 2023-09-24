e0edc73b6311c812dc7d4bb13cca749841c65509
eduarchilon <eduarchilon@outlook.es>
2023-09-23 23:02:40 -0300

    feat(test) create app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AuthModule } from './modules/auth/auth.module';
+import { CalendarModule } from './modules/calendar/calendar.module';
+import { HomeModule } from './modules/home/home.module';
+import { HomeRoutingModuleModule } from './modules/home/home-routing-module.module';
+import { AuthRoutingModule } from './modules/auth/auth-routing.module';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
-xdescribe('AppComponent', () => {
+describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        RouterTestingModule,
+        AuthModule,
+        CalendarModule,
+        HomeModule,
+        HomeRoutingModuleModule,
+        AuthRoutingModule,
+        CommonModule,
+        BrowserModule,
+      ],
       declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     })
   );
 
