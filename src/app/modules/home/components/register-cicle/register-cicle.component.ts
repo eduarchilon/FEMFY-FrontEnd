d1755097e2b3bb5c2964b79e16b7a9c87b18c388
eduarchilon <eduarchilon@outlook.es>
2023-10-11 01:39:59 -0300

    feat(cycle) register cycle user

--- src/app/modules/home/components/register-cicle/register-cicle.component.ts
+++ src/app/modules/home/components/register-cicle/register-cicle.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { setCycle } from 'src/app/redux/actions/cycle.action';
 import { selectUserLogin } from 'src/app/redux/selectors/login.selector';
 import { AppState } from 'src/app/redux/store/app.store';
 import { CicleService } from 'src/app/services/cicle/cicle.service';
+import { SpinnerService } from 'src/app/services/spinner/spinner.service';
 
 @Component({
   selector: 'app-register-cicle',
@@ -17,15 +19,19 @@ export class RegisterCicleComponent implements OnInit {
 
   formCycle: FormGroup = new FormGroup({
     dateBeging: new FormControl('', Validators.required),
-    dateEnd: new FormControl('', Validators.required),
+    daysOfBleeding: new FormControl('', Validators.required),
   });
 
   idUser!: number;
 
+  spinnerConsumer: string = 'RegisterCicleComponent';
+
   constructor(
     private dialogRef: MatDialogRef<RegisterCicleComponent>,
     private cicleService: CicleService,
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    private spinnerService: SpinnerService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -43,14 +49,17 @@ export class RegisterCicleComponent implements OnInit {
     this.cicleService
       .registerCycle({
         dateBeging: this.formCycle?.value?.dateBeging,
-        dateEnd: this.formCycle?.value?.dateEnd,
+        daysOfBleeding: this.formCycle?.value?.daysOfBleeding,
         idUser: this.idUser,
       })
       .subscribe({
         next: (response: any) => {
+          this.closeDialog();
           if (response) {
+            this.router.navigate(['/']).then(() => {
+              location.reload();
+            });
             return response;
-            // this.store.dispatch(setCycle({ cycle: response }));
           }
         },
         error: (error) => error,
