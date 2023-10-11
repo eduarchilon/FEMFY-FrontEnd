abd4ab244522c96ddb7095a8e8e10efb9de340f6
eduarchilonopendevpro <chilon.e@opendevpro.com>
2023-10-11 18:10:01 -0300

    feat(foro) create foro

--- src/app/modules/forum/forum.component.ts
+++ src/app/modules/forum/forum.component.ts
@@ -1,26 +1,47 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { SpinnerService } from 'src/app/services/spinner/spinner.service';
 
 @Component({
   selector: 'app-forum',
   templateUrl: './forum.component.html',
-  styleUrls: ['./forum.component.scss']
+  styleUrls: ['./forum.component.scss'],
 })
 export class ForumComponent {
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private spinnerService: SpinnerService,
+    private router: Router
+  ) {}
+
+  spinnerConsumer: string = 'ForumComponent';
 
   apiUrl: string = 'https://651f0a5044a3a8aa47695bd0.mockapi.io/api/topic';
   topics: any[] = [];
 
   ngOnInit() {
-    this.getTopics().subscribe(data => {
-      this.topics = data;
+    this.getTopics().subscribe({
+      next: (response: any) => {
+        console.log(response);
+        this.spinnerService.showProgressSpinner(this.spinnerConsumer);
+        if (response) {
+          this.topics = response;
+          this.spinnerService.hideProgressSpinner(this.spinnerConsumer);
+        } else {
+          this.spinnerService.hideProgressSpinner(this.spinnerConsumer);
+        }
+      },
     });
   }
 
   getTopics(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any[]>(this.apiUrl).pipe(
+      map((response) => {
+        return response;
+      })
+    );
   }
 
   getImageUrl(imgPath: string): string {
@@ -30,5 +51,4 @@ export class ForumComponent {
   getRedirectUrl(topicId: number): string {
     return `/foro/${topicId}`;
   }
-
 }
