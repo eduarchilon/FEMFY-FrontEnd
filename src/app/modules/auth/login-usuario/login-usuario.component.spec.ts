9d699c851972c61d4636714e5a99ad0e229339e3
Agustina Luciana Gimenez <agus.luu@gmail.com>
2023-10-27 20:56:50 -0300

    auth test

--- src/app/modules/auth/login-usuario/login-usuario.component.spec.ts
+++ src/app/modules/auth/login-usuario/login-usuario.component.spec.ts
@@ -1,21 +1,83 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginUsuarioComponent } from './login-usuario.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Store } from '@ngrx/store';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoaderService } from 'src/app/services/loader/loader.service';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 
-xdescribe('LoginUsuarioComponent', () => {
+fdescribe('LoginUsuarioComponent', () => {
   let component: LoginUsuarioComponent;
   let fixture: ComponentFixture<LoginUsuarioComponent>;
 
+  let authService: jasmine.SpyObj<AuthService>;
+  let loaderService: jasmine.SpyObj<LoaderService>;
+
   beforeEach(() => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['login']);
+    const loaderServiceSpy = jasmine.createSpyObj('LoaderService', [
+      'showLoader',
+      'hideLoader',
+    ]);
+
     TestBed.configureTestingModule({
-      declarations: [LoginUsuarioComponent]
+      declarations: [LoginUsuarioComponent],
+      imports: [
+        HttpClientModule,
+        MatDialogModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: LoaderService, useValue: loaderServiceSpy },
+        { provide: Store, useValue: {} },
+      ],
     });
     fixture = TestBed.createComponent(LoginUsuarioComponent);
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    loaderService = TestBed.inject(
+      LoaderService
+    ) as jasmine.SpyObj<LoaderService>;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('loginUser()', () => {
+    const authenticatedUser = {
+      idUser: 61,
+      typeUserID: 1,
+      firstName: null,
+      lastName: null,
+      userName: 'ana',
+      password: 'ana',
+      isSuscriptor: null,
+      birthdate: '1992-10-15',
+      phone: null,
+      email: 'ana@gmail.com',
+      emotion: null,
+      localidad: null,
+      avatar: null,
+    };
+    authService.login.and.returnValue(of(authenticatedUser));
+    component.formLogin.setValue({
+      userName: authenticatedUser.userName,
+      password: authenticatedUser.password,
+    });
+
+    component.loginUser();
+
+    expect(loaderService.showLoader).toHaveBeenCalled();
+    expect(authService.login).toHaveBeenCalledWith(
+      authenticatedUser.userName,
+      authenticatedUser.password
+    );
   });
 });
--- src/app/modules/auth/registro-usuario/registro-usuario.component.spec.ts
+++ src/app/modules/auth/registro-usuario/registro-usuario.component.spec.ts
@@ -1,15 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegistroUsuarioComponent } from './registro-usuario.component';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { LoaderService } from 'src/app/services/loader/loader.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
-xdescribe('RegistroUsuarioComponent', () => {
+describe('RegistroUsuarioComponent', () => {
   let component: RegistroUsuarioComponent;
   let fixture: ComponentFixture<RegistroUsuarioComponent>;
 
+  let authService: jasmine.SpyObj<AuthService>;
+  let loaderService: jasmine.SpyObj<LoaderService>;
+
   beforeEach(() => {
+    authService = jasmine.createSpyObj('AuthService', ['register']);
+    loaderService = jasmine.createSpyObj('LoaderService', ['showLoader', 'hideLoader']);
+
     TestBed.configureTestingModule({
-      declarations: [RegistroUsuarioComponent]
+      declarations: [RegistroUsuarioComponent],
+      imports: [
+        HttpClientModule,
+        MatDialogModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        { provide: AuthService, useValue: authService },
+        { provide: LoaderService, useValue: loaderService },
+      ],
     });
+
     fixture = TestBed.createComponent(RegistroUsuarioComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +40,41 @@ xdescribe('RegistroUsuarioComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('signupUser()', () => {
+    const authenticatedUser = {
+      idUser: 61,
+      typeUserID: 1,
+      firstName: null,
+      lastName: null,
+      userName: 'ana',
+      password: 'ana',
+      isSuscriptor: null,
+      birthdate: '1992-10-15',
+      phone: null,
+      email: 'ana@gmail.com',
+      emotion: null,
+      localidad: null,
+      avatar: null,
+    };
+
+    authService.register.and.returnValue(of(authenticatedUser));
+    component.formRegister.setValue({
+      userName: authenticatedUser.userName,
+      password: authenticatedUser.password,
+      birthdate: authenticatedUser.birthdate,
+      email: authenticatedUser.email,
+    });
+
+    component.signupUser();
+
+    expect(loaderService.showLoader).toHaveBeenCalled();
+    expect(authService.register).toHaveBeenCalledWith({
+      userName: authenticatedUser.userName,
+      password: authenticatedUser.password,
+      birthdate: authenticatedUser.birthdate,
+      email: authenticatedUser.email,
+    });
   });
 });
