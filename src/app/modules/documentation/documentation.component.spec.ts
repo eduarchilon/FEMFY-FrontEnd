c860eaf7da16a5cc62711889dcc0621923a2cea8
Agustina Luciana Gimenez <agus.luu@gmail.com>
2023-10-28 02:10:35 -0300

    tests

--- src/app/modules/documentation/documentation.component.spec.ts
+++ src/app/modules/documentation/documentation.component.spec.ts
@@ -1,20 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DocumentationComponent } from './documentation.component';
-xdescribe('DocumentationComponent', () => {
-  let component: DocumentationComponent;
-  let fixture: ComponentFixture<DocumentationComponent>;
+// import { DocumentationComponent } from './documentation.component';
+// import { HttpClientModule } from '@angular/common/http';
+// import { StorageModule } from '@angular/fire/storage';
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [DocumentationComponent],
-    });
-    fixture = TestBed.createComponent(DocumentationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+// fdescribe('DocumentationComponent', () => {
+//   let component: DocumentationComponent;
+//   let fixture: ComponentFixture<DocumentationComponent>;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [DocumentationComponent],
+//       imports: [HttpClientModule, StorageModule,  ],
+//       providers:[]
+//     });
+//     fixture = TestBed.createComponent(DocumentationComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
