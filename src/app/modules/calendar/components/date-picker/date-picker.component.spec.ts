c4b4c5c163c8683c442c90e859106e384bf78891
eduarchilon <eduarchilon@outlook.es>
2023-11-16 14:33:46 -0300

    fix test index and landing

--- src/app/modules/calendar/components/date-picker/date-picker.component.spec.ts
+++ src/app/modules/calendar/components/date-picker/date-picker.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DatePickerComponent } from './date-picker.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { CalendarModule } from '../../calendar.module';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { HttpClientModule } from '@angular/common/http';
+import { Store } from '@ngrx/store';
 
-xdescribe('DatePickerComponent', () => {
+fdescribe('DatePickerComponent', () => {
   let component: DatePickerComponent;
   let fixture: ComponentFixture<DatePickerComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DatePickerComponent]
+      declarations: [DatePickerComponent],
+      imports: [
+        MatDialogModule,
+        CalendarModule,
+        MatDatepickerModule,
+        HttpClientModule,
+      ],
+      providers: [{ provide: Store, useValue: {} }],
     });
     fixture = TestBed.createComponent(DatePickerComponent);
     component = fixture.componentInstance;
--- src/app/modules/home/landing/landing.component.spec.ts
+++ src/app/modules/home/landing/landing.component.spec.ts
@@ -1,15 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LandingComponent } from './landing.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { of } from 'rxjs';
 
-xdescribe('LandingComponent', () => {
+fdescribe('LandingComponent', () => {
   let component: LandingComponent;
   let fixture: ComponentFixture<LandingComponent>;
+  let authService: AuthService;
+
+  beforeEach(async () => {
+    authService = {
+      login: jasmine.createSpy('login').and.returnValue(of({})),
+    } as unknown as AuthService;
+    await TestBed.configureTestingModule({
+      declarations: [LandingComponent],
+      imports: [HttpClientModule],
+      providers: [{ provide: AuthService, useValue: authService }],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [LandingComponent]
-    });
     fixture = TestBed.createComponent(LandingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
