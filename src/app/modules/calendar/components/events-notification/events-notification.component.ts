6a161df984a4053dd5e354b2dff0d18f81b510a5
eduarchilon <eduarchilon@outlook.es>
2023-11-18 19:52:51 -0300

    fix event notification

--- src/app/modules/calendar/components/events-notification/events-notification.component.ts
+++ src/app/modules/calendar/components/events-notification/events-notification.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import * as moment from 'moment';
 import { EventCalendar } from 'src/app/models/event-calendar.model';
@@ -9,15 +15,47 @@ import { NotificationService } from 'src/app/services/notification/notification.
   templateUrl: './events-notification.component.html',
   styleUrls: ['./events-notification.component.scss'],
 })
-export class EventsNotificationComponent implements OnInit {
+export class EventsNotificationComponent implements OnInit, OnChanges {
   @Input() events: EventCalendar[] = [];
 
   constructor(
     private notificationService: NotificationService,
     private router: Router
   ) {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.events);
+    this.events.forEach((event: EventCalendar | any) => {
+      setInterval(() => {
+        //La fecha de hoy y las horas
+        const ahora = new Date();
+        const horaActual = ahora.getHours();
+        const minutosActuales = ahora.getMinutes();
+
+        //Obtengo la hora y minutos
+        const time = event?.hour?.split(':');
+        let hour = 0;
+        let minutes = 0;
+        if (event?.hour && time.length === 2) {
+          hour = parseInt(time[0], 10);
+          minutes = parseInt(time[1], 10);
+        }
+
+        const eventDay = moment(event?.date);
+
+        if (
+          moment(ahora).isSame(eventDay, 'day') &&
+          horaActual === hour &&
+          minutosActuales === minutes
+        ) {
+          this.sendNotification(event?.title, event?.description);
+        }
+      }, 60000);
+    });
+  }
 
   ngOnInit(): void {
+    console.log(this.events);
+
     this.events.forEach((event: EventCalendar | any) => {
       setInterval(() => {
         //La fecha de hoy y las horas
